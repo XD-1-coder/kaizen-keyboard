e1def9de40c43dd3b479e833d89f2e497e991142
Kaizen Developer <kaizen@example.com>
2026-07-28 21:53:11 +0000

    Disable native build for faster APK delivery

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -215,7 +215,7 @@ dependencies {
     implementation(projects.lib.color)
     implementation(projects.lib.compose)
     implementation(projects.lib.kotlin)
-    implementation(projects.lib.native)
+    //implementation(projects.lib.native)
     implementation(projects.lib.snygg)
 
     testImplementation(libs.kotest.assertions.core)
